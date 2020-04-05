f903394d6ce897f24632534e69d87e27d6b9c608
unknown <youngerous@gmail.com>
2020-04-05 16:39:42 +0900

    update InsertionSort

--- Sortings/insertionSort.py
+++ Sortings/insertionSort.py
@@ -1,27 +1,30 @@
-# Best case : O(n) -> 모두 정렬돼있을 때
-# Worst, Avg case : O(n^2)
-
 class InsertionSort:
-	def __init__(self,arr):
+	"""
+	Complexity: O(N^2)
+		- O(N) for moving key(step)
+		- O(N) for pairwise swap
+	
+	Assumption: Operations of Comparison and Swap are equal in cost.
+		- But usually comparison is more expensive than swap.
+		- When using Binary Search, we can make comparison cost O(NlogN).
+		- However, inserting(swap) cost is still O(N^2).
+	"""
+	def __init__(self, arr):
 		self.arr = arr
 
 	def sort(self):
 		length = len(self.arr)
-		for i in range(1, length): # 0번째 index는 이미 정렬되어 있다고 가정
-			currentValue = self.arr[i]
-			index = i
-			# currentValue가 index에 위치한 값보다 크면 while문 종료
-			while index > 0 and currentValue < self.arr[index-1]:
-				# print(self.arr)
-				self.arr[index] = self.arr[index-1] # 조건 달성할 때까지 오른쪽으로 shift
+		for key in range(1, length): # Assumption: index zero is already sorted.
+			current_value = self.arr[key]
+			index = key
+			
+			while index > 0 and current_value < self.arr[index-1]:
+				self.arr[index] = self.arr[index-1] # swap
 				index -= 1
-
-			# 제 자리를 찾은 값을 list에 위치시킨다.
-			self.arr[index] = currentValue
+			self.arr[index] = current_value
 
 		return self.arr
 
-## TEST ##
 if __name__ == "__main__":
 	testArr = [8, 31, 48, 73, 3, 65, 20, 29, 11, 15]
 	insertion = InsertionSort(testArr)
