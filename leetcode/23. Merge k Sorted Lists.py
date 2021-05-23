0891dc54826c7aedb989100c7d808f497e84219c
youngerous <youngerous@gmail.com>
2021-05-23 21:38:01 +0900

    priority queue using heapq library

---
+++ leetcode/23. Merge k Sorted Lists.py
@@ -0,0 +1,30 @@
+import heapq
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        root = result = ListNode(None)
+        heap = []
+
+        # 각 연결 리스트의 루트를 힙에 저장
+        # heapq 라이브러리는 파이썬의 리스트를 최소 힙처럼 다룰 수 있도록 함
+        # ref: https://www.daleseo.com/python-heapq/
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+        # 힙 추출 이후 다음 노드는 다시 저장
+        while heap:
+            node = heapq.heappop(heap)  # 가장 작은 원소 삭제 후 반환
+            idx = node[1]
+            result.next = node[2]
+
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+
+        return root.next
